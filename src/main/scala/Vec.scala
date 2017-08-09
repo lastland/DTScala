3b97c9979f04732644ac1e3cd6d26e1e88e45c7a
Yao Li <hnkfliyao@gmail.com>
2017-08-08 21:25:59 -0400

    Rep.

--- src/main/scala/Vec.scala
+++ src/main/scala/Vec.scala
@@ -1,5 +1,6 @@
 package dependent.vectors
 import scala.language.higherKinds
+import scala.language.implicitConversions
 import dependent.nat._
 
 sealed trait Vec[N <: Nat] {
@@ -17,4 +18,22 @@ case class Cons[N <: Nat](h: Int, t: Vec[N])
     extends Vec[Succ[N]] {
   type NM[M <: Nat] = Succ[t.NM[M]]
   def app[M <: Nat](b: Vec[M]) = Cons(h, t.app(b))
+}
+
+object Rep {
+  sealed trait VecVal[N <: Nat] {
+    def cons(x: Int): Vec[N]
+  }
+
+  implicit def repz : VecVal[Z.type] = new VecVal[Z.type] {
+    def cons(x: Int) = Nil
+  }
+
+  implicit def repn[N <: Nat] (implicit pv: VecVal[N]) :
+      VecVal[Succ[N]] = new VecVal[Succ[N]] {
+    def cons(x: Int) = Cons(x, pv.cons(x))
+  }
+
+  def rep[N <: Nat](n: N, x: Int)(implicit v: VecVal[N]): Vec[N] =
+    v.cons(x)
 }
